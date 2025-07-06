c4e9f88a70bf28ae58fbbef04339a680b22865e8
JPEDU <jamesperkinsedu@gmail.com>
2025-07-07 00:09:20 +0100

    Accolade Loops for Users Badges
    This system now loops through the badges a user has, then checks against the badges table and pulls relevant data.

--- components/curraco.php
+++ components/curraco.php
@@ -9,52 +9,37 @@ include 'INC/config.php'; // Config
 // Connect to Database
 $conn = new mysqli($DBIPAD, $DBUSER, $DBPASS,$DBINFO);
 $sql = "SELECT * FROM $iii WHERE usernick='MickeyMouse' ";
-$results1 = $conn->query($sql);
-    if($results1->num_rows > 0) 
+$user = $conn->query($sql);
+    if($user->num_rows > 0) 
     {
-        while($row = $results1->fetch_assoc()) // Get the USER //
+        while($row = $user->fetch_assoc()) // Get the USER //
         {
             $currbadges = $row["badges"];
-            // Gets Individual Badge Data //
-            $sepbadges1 = explode(",",$currbadges);
-            foreach ($sepbadges1 as $badge1)
+            $sepbadges = explode(',',$currbadges);
+            foreach ($sepbadges as $badge)
             {
-                echo trim($badge1) . "</br>";
-            }
-            // REMOVE AFTER DEBUG //
+                $badge = trim($badge);
 
-            // TODO - Make this section do a loop -- Maybe need to remove the while and instead just have it loop indivdually?
-            $sql2 = "SELECT * FROM $bads WHERE badname='$badge'";
-            $result2 = $conn->query($sql2);
-            if ($result2->num_rows > 0) 
-            {
+                $stmt = $conn->prepare("SELECT * FROM badges WHERE badname=?");
+                $stmt->bind_param("s",$badge);
+                $stmt->execute();
 
-            while ($row2 = $result2->fetch_assoc()) 
-            {
-                echo'
-                <div class="card">
-                        <img style="width:100px;height:100px;" src="'.$row2["badimg"].'" alt="'.$row2["badimgalt"].'">
-                        <div class="info">
-                            <h3>'.$row2["badname"].'</h3>
-                            <p>[Points %]</p>
-                            <a href="">Next goal</a>
-                        </div>
+                $badgeres = $stmt->get_result();
+                while($cbadge = $badgeres->fetch_assoc())
+                {
+                    echo'
+                    <div class="card">
+                            <img style="width:100px;height:100px;" src="'.$cbadge["badimg"].'" alt="'.$cbadge["badimgalt"].'"">
+                            <div class="info">
+                                <h3>'.$cbadge["badname"].'</h3>
+                                <p>[Points %]</p>
+                                <a href="">Next goal</a>
+                            </div>
                     </div>
-                ';
-            }
-            }
-            /*
-            echo'
-            <div class="card">
-                    <img style="width:100px;height:100px;" src="res/bads/devtester.png">
-                    <div class="info">
-                        <h3>[Badge Title]</h3>
-                        <p>[Points %]</p>
-                        <a href="">Next goal</a>
-                    </div>
-                </div>
             ';
-            */
+                }
+                $stmt->close();
+            }
         }
     }
 ?>
